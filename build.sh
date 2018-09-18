80ace6bc77155168c3036d4e09f0675d3829c84b
Alexander Brock <alexander@lunar-orbit.de>
2018-09-18 14:03:08 +0200

    Minimal reproducer

---
+++ build.sh
@@ -0,0 +1,1 @@
+/usr/bin/c++ -fPIC -O3 -Wall -pedantic -g -rdynamic main.cpp  -o callgrind-test -Wl,-rpath,/usr/local/lib /usr/local/lib/libopencv_sfm.so.4.0.0
--- main.cpp
+++ main.cpp
@@ -4,15 +4,23 @@ using namespace std;
 
 void func1() {
     size_t sum = 0;
-    for (size_t ii = 0; ii < 1e6; ++ii) {
-        sum += ii;
+//#pragma omp parallel for
+    for (size_t ii = 0; ii < 1000000; ++ii) {
+        size_t t1 = ii;
+        t1 += 1;
+        t1 *= 2;
+        t1 /= 3;
+//#pragma omp critical
+        {
+            sum += t1;
+        }
     }
     std::cout << "The sum is: " << sum << std::endl;
 }
 
 void func2() {
     size_t sum = 0;
-    for (size_t ii = 0; ii < 1e4; ++ii) {
+    for (size_t ii = 0; ii < 10000; ++ii) {
         sum += ii;
     }
     std::cout << "The sum is: " << sum << std::endl;
@@ -21,7 +29,13 @@ void func2() {
 int main()
 {
     cout << "Hello World!" << endl;
+
+//#pragma omp parallel sections
+    {
+//#pragma omp section
     func1();
+//#pragma omp section
     func2();
+    }
     return 0;
 }
